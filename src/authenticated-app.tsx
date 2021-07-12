4f4a76316494797cf13f7b5d2c01799c2c98b7f8
sakumashirayuki <2909549511@qq.com>
2021-07-12 16:38:20 +0800

    style: create custom Row component

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,13 +1,40 @@
 import React from "react";
 import { useAuth } from "context/auth-context";
 import { ProjectListScreen } from "screens/project-list";
+import styled from "@emotion/styled";
+import { Row } from "components/lib";
 
 export const AuthenticatedApp = () => {
   const { logout } = useAuth();
   return (
     <div>
-      <button onClick={logout}>登出</button>
-      <ProjectListScreen />
+      <PageHeader between={true}>
+        <HeaderLeft gap={true}>
+          <h3>logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </PageHeader>
+      <Main>
+        <ProjectListScreen />
+      </Main>
     </div>
   );
 };
+
+// const HeaderItem = styled.h3`
+//   margin-right: 3rem;
+// `;
+
+const PageHeader = styled(Row)``;
+
+const HeaderLeft = styled(Row)``;
+
+const HeaderRight = styled.div``;
+
+const Main = styled.main`
+  height: calc(100vh - 6rem);
+`;
